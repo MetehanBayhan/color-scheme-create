6133c7c45ec2797b9ec9a9ad0cb63a8c2778ae70
MetehanBayhan <metehnb@gmail.com>
2023-04-28 23:44:49 +0300

    creating the logic

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-fetch("https://www.thecolorapi.com/scheme?hex=0047AB&format=html&mode=analogic&count=6")
-  .then(res => res)
-  .then(data => console.log(data))
+const getBtn = document.querySelector(".get-btn")
+const colorInput = document.querySelector(".color-picker")
+const dropdownMenu = document.getElementById("color-dropdown")
+
+
+
+
+
+getBtn.addEventListener("click", () => {
+  getScheme()
+})
+
+
+function getScheme() {
+  if(dropdownMenu.value != "" && colorInput) {
+    fetch(`https://www.thecolorapi.com/scheme?hex=${colorInput.value.substring(1)}&mode=${dropdownMenu.value}&count=5`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data.colors)
+      })
+  }
+}
