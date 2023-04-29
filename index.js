687f535e0e26cdcad6dab53241841ed070b458c3
MetehanBayhan <metehnb@gmail.com>
2023-04-29 09:30:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,16 +7,31 @@ const dropdownMenu = document.getElementById("color-dropdown")
 
 
 getBtn.addEventListener("click", () => {
+  getBtn.disabled = true
   getScheme()
+  console.log(colorSchemeArray);
+  setTimeout(renderColors, 1000)
 })
 
-
+let colorSchemeArray = []
 function getScheme() {
   if(dropdownMenu.value != "" && colorInput) {
     fetch(`https://www.thecolorapi.com/scheme?hex=${colorInput.value.substring(1)}&mode=${dropdownMenu.value}&count=5`)
       .then(res => res.json())
       .then(data => {
-        console.log(data.colors)
+        for (let i = 0; i < 5 ; i++){
+          colorSchemeArray.push(data.colors[i].hex.value);  
+        }
       })
   }
 }
+
+function renderColors() {
+  for (let i = 0; i < colorSchemeArray.length ; i++){
+    document.querySelector(`.color-${i+1}`).style.backgroundColor = colorSchemeArray[i]
+    document.querySelector(`.code-${i+1}`).textContent = colorSchemeArray[i]
+  }
+  colorSchemeArray = []
+  getBtn.disabled = false
+}
+
